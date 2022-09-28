644d88df9e3e6e7529a927c851b8cc18643c08a8
brunop97 <bruno.peroni97@gmail.com>
2022-09-28 10:30:34 -0300

    Versao com erro

--- planetas_fragments/app/src/main/java/com/example/planetas_fragments/LeftFragment.java
+++ planetas_fragments/app/src/main/java/com/example/planetas_fragments/LeftFragment.java
@@ -1,64 +1,69 @@
 package com.example.planetas_fragments;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LeftFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LeftFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LeftFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LeftFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LeftFragment newInstance(String param1, String param2) {
-        LeftFragment fragment = new LeftFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView lvPlanetas;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState) {
+
+        View view = inflater.inflate( R.layout.fragment_left, null );
+
+        lvPlanetas = (ListView) view.findViewById( R.id.lvPlanetas );
+
+        lvPlanetas.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long id) {
+
+                tratarSelecao( position );
+
+            }
+
+        } );
+
+        return view;
+    }//fim do método onCreateView
+
+    protected void tratarSelecao( int position ) {
+
+        Configuration config = getResources().getConfiguration();
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            androidx.fragment.app.FragmentManager fm = getFragmentManager();
+
+            RightFragment fd = (RightFragment) fm.findFragmentById( R.id.fragmentRight );
+
+            fd.setConteudo( position );
+
+        } else {
+            RightFragment fragDir = new RightFragment();
+
+            Bundle args = new Bundle();
+            args.putInt("pos", position);
+            fragDir.setArguments(args);
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.add(android.R.id.content, fragDir);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+
         }
-    }
+    } //fim do método tratarSelecao
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_left, container, false);
-    }
-}
+} //fim da classe LeftFragment
--- planetas_fragments/app/src/main/java/com/example/planetas_fragments/MainActivity.java
+++ planetas_fragments/app/src/main/java/com/example/planetas_fragments/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.planetas_fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Configuration config = getResources().getConfiguration();
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView( R.layout.activity_main );
+        }
+        else{
+            LeftFragment lf = new LeftFragment();
+
+            FragmentManager fragmentManager = getFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.add(android.R.id.content, lf);
+
+            fragmentTransaction.commit();
+        }
     }
 }
